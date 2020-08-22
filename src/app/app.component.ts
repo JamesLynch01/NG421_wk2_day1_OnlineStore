7824ee3925ba647c9b2f455074f16b0fa9ae9684
JamesLynch01 <Jamesblynch94@gmail.com>
2020-08-22 13:57:23 -0500

    I did some stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProduct } from './iproduct';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  products: Array<IProduct>;
+
+  constructor(private ProductService: ProductService){
+
+  }
+  ngOnInit() {
+    this.products = this.ProductService.getProducts();
+  }
 }
